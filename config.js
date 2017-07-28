a159ed1bfe31823a64f66a3507dea22224e65032
Gabor Molnar <molnr.gabor@gmail.com>
2017-07-28 07:51:24 +0200

    Bugfix added

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
+    'secretKey': '',
     //'mongoUrl' : 'mongodb://localhost:27017/financeplanner',
-    //'secretKey': '58fb3b0e9e77d8001400257e',
-    'mongoUrl' : 'mongodb://financePlanner:Macskafarm123@sl-us-dal-9-portal.3.dblayer.com:23442/financeplannerdb?ssl=true',
+    'mongoUrl' : '',
     'facebook': {
-        clientID: '729651967195752',
-        clientSecret: 'be89352c6ee7a05fd5efc022d563d214',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
+        clientID: '',
+        clientSecret: '',
+        callbackURL: ''
     }
 }
